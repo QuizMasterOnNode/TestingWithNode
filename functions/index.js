c1e1e16e244dfe36b007289b2a4298d3d019b39e
Yasir Tahir <yasirtahir@lewisu.edu>
2022-04-03 00:42:29 -0500

    delpoyed with node.

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,34 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
-
+//const router = express.Router();
+//const path = require("path");
+/*
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/test.html"));
+});
+*/
 app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/static/test.html");
+    res.sendFile(__dirname + "/static/index.html");
 });
 
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/static/about.html");
+});
+
+app.get("/hello", (req, res) => {
+    res.send("Hi there!");
+});
+
+/*
+app.get("/hello", (req, res) => {
+    res.send("hello");
+});
+*/
 app.get("/api", (req, res) => {
     res.json({ bongs: "BONG ".repeat(hours) });
 });
-
 
 exports.app = functions.https.onRequest(app);
